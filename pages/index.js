b7e5586aaa27e4fe09eae0a38a68edeb942722ab
voxinraleigh <lesterwong1976@gmail.com>
2023-05-31 16:11:26 -0400

    New revisions by Lster

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,66 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+const [userInput, setUserInput] = useState('');
+
+const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+{/* new code for API Call */}
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
+
+const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+};
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Blog Content Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Powered by ChatGPT</h2>
           </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder="start typing here"
+            value={userInput}
+            onChange={onUserChangedText}
+            />
+            {/* Generator Button! */}
+         <div className="prompt-buttons">
+             <a className="generate-button" onClick={callGenerateEndpoint}>
+               <div className="generate">
+                 <p>Generate</p>
+               </div>
+             </a>
+           </div>
         </div>
       </div>
       <div className="badge-container grow">

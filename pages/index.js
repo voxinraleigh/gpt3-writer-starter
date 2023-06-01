c521245a05e58d96e3d908bbb1575bd2b3fba122
voxinraleigh <lesterwong1976@gmail.com>
2023-05-31 23:09:02 -0400

    New revisions by Lester 5-31-2023 11:08pm working copy

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,37 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
-import { useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
-const [userInput, setUserInput] = useState('');
+    const [userInput, setUserInput] = useState('');
+    const [apiOutput, setApiOutput] = useState('')
+    const [isGenerating, setIsGenerating] = useState(false)
 
-const [apiOutput, setApiOutput] = useState('')
-const [isGenerating, setIsGenerating] = useState(false)
+    {/* new code for API Call */}
+    const callGenerateEndpoint = async () => {
+        setIsGenerating(true);
+        console.log("Calling OpenAI...")
+        const response = await fetch("/api/generate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userInput }),
+        });
 
-{/* new code for API Call */}
-const callGenerateEndpoint = async () => {
-  setIsGenerating(true);
-  console.log("Calling OpenAI...")
-  const response = await fetch('/api/generate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ userInput }),
-  });
+        const data = await response.json();
+        const { output } = data;
+        console.log("OpenAI replied...", output.text)
 
-  const data = await response.json();
-  const { output } = data;
-  console.log("OpenAI replied...", output.text)
-
-  setApiOutput(`${output.text}`);
-  setIsGenerating(false);
-}
+        setApiOutput(`${output.text}`);
+        setIsGenerating(false);
+    }
 
 
-const onUserChangedText = (event) => {
+    const onUserChangedText = (event) => {
     setUserInput(event.target.value);
-};
+    };
 
   return (
     <div className="root">
@@ -54,15 +53,29 @@ const onUserChangedText = (event) => {
             onChange={onUserChangedText}
             />
             {/* Generator Button! */}
-         <div className="prompt-buttons">
+            <div className="prompt-buttons">
              <a className="generate-button" onClick={callGenerateEndpoint}>
                <div className="generate">
                  <p>Generate</p>
                </div>
              </a>
            </div>
+           {/* New code I added here */}
+             {apiOutput && (
+             <div className="output">
+               <div className="output-header-container">
+                 <div className="output-header">
+                   <h3>Output</h3>
+                 </div>
+               </div>
+               <div className="output-content">
+                 <p>{apiOutput}</p>
+               </div>
+             </div>
+           )}
+           </div>
         </div>
-      </div>
+
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
